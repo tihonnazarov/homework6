52d406c4599f7f5d19d3cfab5c19890b52d4641f
tihonnazarov <139996570+tihonnazarov@users.noreply.github.com>
2023-08-02 03:49:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Slider from 'react-slick';
+import styled from 'styled-components';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const SliderContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const SlideImage = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+`;
+
+const PhotoSlider = () => {
+    const photos = [
+        'https://image.arrivalguides.com/500x500/03/7bf557a6b37438430b62dde70af75256.jpg',
+        'https://img1.cgtrader.com/items/3890856/4c882e6699/large/360-degree-snowy-mountains-3d-model-obj-stl-blend.jpg',
+        'https://thumbor.bigedition.com/img/FYumbNUre2r0odGdoCUlFmsh5MA=/500x500/filters:format(webp):quality(80)/granite-web-prod/7a/4d/7a4d56649aab4fafbf8764d29146aae4.jpg',
+
+    ];
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
+    return (
+        <SliderContainer>
+            <Slider {...settings}>
+                {photos.map((photoUrl, index) => (
+                    <div key={index}>
+                        <SlideImage src={photoUrl} alt={`Slide ${index + 1}`}/>
+                    </div>
+                ))}
+            </Slider>
+        </SliderContainer>
+    );
+};
+
+export default PhotoSlider;
+
